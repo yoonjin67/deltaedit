d51c2009556a45c9a723fc9d2746dc716d773632
Yunjin Lee <yoonjin671@protonmail.ch>
2019-06-16 17:35:19 +0900

    Initial commit

--- Installer.py
+++ Installer.py
@@ -9,7 +9,6 @@ class WIN(Gtk.Window):
 		Gtk.Window.__init__(self, title="INSTALLER")
 		grid=Gtk.Grid()
 		self.add(grid)
-		pre_installation=Gtk.Button("Click before Installation")
 		ko=Gtk.Button("KOREAN")
 		jp=Gtk.Button("JAPANESE")
 		cn=Gtk.Button("CHINESE")
@@ -18,12 +17,10 @@ class WIN(Gtk.Window):
 		grid.attach_next_to(jp,ko,Gtk.PositionType.RIGHT,1,1)
 		grid.attach_next_to(cn,jp,Gtk.PositionType.RIGHT,1,1)
 		grid.attach_next_to(all,jp,Gtk.PositionType.BOTTOM,1,1)
-		grid.attach_next_to(pre_installation,all,Gtk.PositionType.BOTTOM,1,1)
 		ko.connect("clicked", self.kor)
 		jp.connect("clicked",self.jpn)
 		cn.connect("clicked",self.chn)
 		all.connect("clicked", self.INST_ALL)
-		pre_installation.connect("clicked", self.CHMOD)
 	def INST_ALL(self,widget):
 		try:
 			call('./Install.sh')
@@ -88,8 +85,6 @@ class WIN(Gtk.Window):
 			w.connect("destroy", Gtk.main_quit)
 			w.show_all()
 			Gtk.main()
-	def CHMOD(self,widget):
-		Popen("sudo chmod 777 /etc/dedit/output && sudo chmod 777 /etc/dedit/.dedit_cfunc.c")
 win=WIN()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
