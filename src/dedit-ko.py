148cea5e9a11c8a5ddbeadc0bf78a573515a595d
Yunjin Lee <yoonjin671@protonmail.ch>
2019-06-22 02:04:47 +0000

    Update dedit-ko.py

--- src/dedit-ko.py
+++ src/dedit-ko.py
@@ -8,9 +8,6 @@ gi.require_version('WebKit', '3.0')
 gi.require_version('GtkSource', '4')
 from gi.repository import Gtk, GtkSource, WebKit
 from urllib.request import urlopen
-import guesslang
-from guesslang import Guess
-##창을 생성하고, 버튼 클릭 시 실행될 함수들을 명시합니다.##
 print("           Keisung/Bit_Time   ")
 print("DeltaEdit____________________0000 0000 0000 0111")
 print("_______________Welcome__________________________")
